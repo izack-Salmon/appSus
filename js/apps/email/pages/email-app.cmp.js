3cc4cb29818d91c7e3956c00d6b5189cd0ac9629
Paz Balter <pazbalterpb@gmail.com>
2021-11-10 12:26:31 +0200

    paz start

--- js/apps/email/pages/email-app.cmp.js
+++ js/apps/email/pages/email-app.cmp.js
@@ -1,8 +1,6 @@
 import { emailService } from '../services/email-services.js';
 export default {
-  template: `
-        <footer class="app-footer">
-            <p>&copy; Coffeerights 2021</p>
-        </footer>
+    template: `
+    
     `,
 };
--- js/apps/email/services/email-services.js
+++ js/apps/email/services/email-services.js
@@ -1,22 +1,67 @@
 import { storageService } from '../../../services/async-storage-service.js';
+const EMAIL_KEY = 'emails';
 export const emailService = {};
+
+
 const loggedinUser = {
-  email: 'user@appsus.com',
-  fullname: 'Mahatma Appsus',
+    email: 'user@appsus.com',
+    fullname: 'Mahatma Appsus',
 };
 
 const email = {
-  id: 'e101',
-  subject: 'Miss you!',
-  body: 'Would love to catch up sometimes',
-  isRead: false,
-  sentAt: 1551133930594,
-  to: 'momo@momo.com',
+    id: 'e101',
+    subject: 'Miss you!',
+    body: 'Would love to catch up sometimes',
+    isRead: false,
+    sentAt: 1551133930594,
+    to: 'momo@momo.com',
 };
 const criteria = {
-  status: 'inbox/sent/trash/draft',
-  txt: 'puki', // no need to support complex text search
-  isRead: true, // (optional property, if missing: show all)
-  isStared: true, // (optional property, if missing: show all)
-  lables: ['important', 'romantic'], // has any of the labels
+    status: 'inbox/sent/trash/draft',
+    txt: 'puki', // no need to support complex text search
+    isRead: true, // (optional property, if missing: show all)
+    isStared: true, // (optional property, if missing: show all)
+    lables: ['important', 'romantic'], // has any of the labels
 };
+
+
+_createBooks()
+
+function query() {
+    return email;
+}
+
+
+
+
+function remove(emailId) {
+    return storageService.remove(EMAIL_KEY, emailId);
+}
+
+function save(email) {
+    if (email.id) return storageService.put(EMAIL_KEY, email);
+    else return storageService.post(EMAIL_KEY, email);
+}
+
+// function addReview(review, bookId) {
+//     return getById(bookId).then(book => {
+//         console.log('from addReview', book)
+//         if (!book.reviews) book.reviews = []
+//         book.reviews.push(review)
+//         return storageService.put(BOOKS_KEY, book) //.then(updatedBook => updatedBook)
+//     })
+// }
+
+
+function getById(emailId) {
+    return storageService.get(EMAIL_KEY, emailId);
+}
+
+
+function _createBooks() {
+    let books = utilService.loadFromStorage(EMAIL_KEY);
+    if (!books || !books.length) {
+        utilService.saveToStorage(EMAIL_KEY, gEmails);
+    }
+    return books;
+}
