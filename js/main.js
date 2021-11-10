1e13044d6a666a05a3296f992d6753e6f766d7fc
izack salomon <hmje123123123@gmail.com>
2021-11-10 10:10:45 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const options = {
+  el: '#app',
+  // router,
+  template: `
+        <section>
+
+        </section>
+    `,
+  components: {},
+};
+
+new Vue(options);
---
+++ js/services/util-service.js
@@ -0,0 +1,42 @@
+export const utilService = {
+  saveToStorage,
+  loadFromStorage,
+  makeId,
+  debounce,
+  getRndInteger,
+};
+
+function saveToStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value) || null);
+}
+
+function loadFromStorage(key) {
+  let data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : undefined;
+}
+
+function makeId(length = 5) {
+  var txt = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return txt;
+}
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function debounce(func, wait) {
+  let timeout;
+
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
