ec2aa754d3a7d1b0494e7cf0fd261e7a73c14db4
izack salomon <hmje123123123@gmail.com>
2021-11-10 12:25:43 +0200

    add header and main route

---
+++ js/cmps/app-header.cmp.js
@@ -0,0 +1,12 @@
+export default {
+  template: `
+        <header class="app-header">
+            <h3>appSus</h3>
+            <nav>
+                <router-link to="/">Home</router-link> |
+                <router-link to="/email">email</router-link> |
+                <router-link to="/keep">keep</router-link> |
+            </nav>
+        </header>
+    `,
+};
