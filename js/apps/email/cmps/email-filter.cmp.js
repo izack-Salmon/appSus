cba256eda0405986f75ae43d68c532221bdfec9d
izack salomon <hmje123123123@gmail.com>
2021-11-11 23:09:59 +0200

    11pm monday

--- js/apps/email/cmps/email-filter.cmp.js
+++ js/apps/email/cmps/email-filter.cmp.js
@@ -1,24 +1,22 @@
 export default {
-    template: `
+  template: `
             <section>
                 <div class="email-top-ruler">
                     <input @input="filter" v-model="filterBy.txt" type="text" placeholder="search mail">
                 </div>
             </section>  
     `,
-    data() {
-        return {
-            filterBy: {
-                txt: '',
-                isRead: false,
-            }
-        }
+  data() {
+    return {
+      filterBy: {
+        txt: '',
+        isRead: false,
+      },
+    };
+  },
+  methods: {
+    filter() {
+      this.$emit('filtered', { ...this.filterBy });
     },
-    methods: {
-        filter() {
-            this.$emit('filtered', {...this.filterBy });
-        }
-
-    },
-
+  },
 };
