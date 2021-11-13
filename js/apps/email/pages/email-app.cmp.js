c75e94c9f637db14664747af939e637e4376534d
Paz Balter <pazbalterpb@gmail.com>
2021-11-14 00:07:29 +0200

    layla bemet tov

--- js/apps/email/pages/email-app.cmp.js
+++ js/apps/email/pages/email-app.cmp.js
@@ -5,7 +5,7 @@ import emailSideRuler from '../cmps/email-side-ruler.cmp.js';
 import emailFilter from '../cmps/email-filter.cmp.js';
 
 export default {
-  template: `
+    template: `
         <main class="app-content">
         <email-sideRuler />
         <div>
@@ -14,97 +14,101 @@ export default {
         </div>
         </main>
     `,
-  data() {
-    return {
-      criteria: {
-        status: 'inbox',
-        txt: '',
-        isRead: null,
-        // isStared: true,
-      },
-      emails: null,
-    };
-  },
-  created() {
-    this.loadEmails();
-    eventBus.$on('new email created', (newEmail) => {
-      return emailService.add(newEmail).then(() => {
+    data() {
+        return {
+            criteria: {
+                status: 'inbox',
+                txt: '',
+                isRead: null,
+                // isStared: true,
+            },
+            emails: null,
+        };
+    },
+    created() {
         this.loadEmails();
-      });
-    });
-    eventBus.$on('mail to delete', (id) => {
-      return emailService.remove(id).then(() => {
-        this.loadEmails();
-      });
-    });
-    eventBus.$on('change status', (folderStr) => {
-      this.criteria.status = folderStr;
-      this.loadEmails();
-    });
-    eventBus.$on('set isRead to true', (id) => {
-      console.log(id);
-      emailService.getById(id).then((email) => {
-        console.log(email);
-        email.isRead = true;
-        emailService.save(email).then(this.loadEmails());
-      });
-    });
-    eventBus.$on('set not read', (id) => {
-      emailService.getById(id).then((email) => {
-        email.isRead = false;
-        emailService.save(email).then(this.loadEmails());
-      });
-    });
-  },
-  methods: {
-    loadEmails() {
-      emailService.query(this.criteria).then((emails) => {
-        this.emails = emails;
-      });
+        eventBus.$on('new email created', (newEmail) => {
+            return emailService.add(newEmail).then(() => {
+                this.loadEmails();
+            });
+        });
+        eventBus.$on('mail to delete', (id) => {
+            return emailService.remove(id).then(() => {
+                this.loadEmails();
+                const msg = { type: 'success', txt: 'Email deleted' };
+                eventBus.$emit('showMsg', msg);
+            });
+        });
+        eventBus.$on('change status', (folderStr) => {
+            this.criteria.status = folderStr;
+            this.loadEmails();
+        });
+        eventBus.$on('set isRead to true', (id) => {
+            console.log(id);
+            emailService.getById(id).then((email) => {
+                console.log(email);
+                email.isRead = true;
+                const msg = { type: 'success', txt: 'Mark as read' };
+                eventBus.$emit('showMsg', msg);
+                emailService.save(email).then(this.loadEmails());
+            });
+        });
+        eventBus.$on('set not read', (id) => {
+            emailService.getById(id).then((email) => {
+                email.isRead = false;
+                emailService.save(email).then(this.loadEmails());
+            });
+        });
     },
-    setFilter(str) {
-      // console.log(str);
-      this.criteria.txt = str;
-      this.loadEmails();
+    methods: {
+        loadEmails() {
+            emailService.query(this.criteria).then((emails) => {
+                this.emails = emails;
+            });
+        },
+        setFilter(str) {
+            // console.log(str);
+            this.criteria.txt = str;
+            this.loadEmails();
+        },
+        setFilterRead(Boolean) {
+            // console.log(Boolean);
+            this.criteria.isRead = Boolean;
+            this.loadEmails();
+        },
     },
-    setFilterRead(Boolean) {
-      // console.log(Boolean);
-      this.criteria.isRead = Boolean;
-      this.loadEmails();
-    },
-  },
-  computed: {
-    emailsToShow() {
-      if (!this.filterBy) return this.emails;
-      const searchStr = this.filterBy.txt.toLowerCase();
-      const emailsToShow = this.emails.filter((email) => {
-        return email.to.toLowerCase().includes(searchStr);
-      });
-      return emailsToShow;
-    },
+    computed: {
+        emailsToShow() {
+            if (!this.filterBy) return this.emails;
+            const searchStr = this.filterBy.txt.toLowerCase();
+            const emailsToShow = this.emails.filter((email) => {
+                return email.to.toLowerCase().includes(searchStr);
+            });
+            return emailsToShow;
+        },
 
-    emailCriteriaToShow() {
-      if (!this.criteria) {
-        const status = this.status;
-        this.emails.filter((email) => {
-          return email.status.includes(status);
-        });
-      } else {
-        const status = this.status;
-        const searchStr = this.criteria.txt.toLowerCase();
-        const emailsToShow = this.emails.filter((email) => {
-          return (
-            email.txt.toLowerCase().includes(searchStr) ||
-            (email.subject.toLowerCase().includes(searchStr) && email.status.includes(status))
-          );
-        });
-        return emailsToShow;
-      }
+        emailCriteriaToShow() {
+            if (!this.criteria) {
+                const status = this.status;
+                this.emails.filter((email) => {
+                    return email.status.includes(status);
+                });
+            } else {
+                const status = this.status;
+                const searchStr = this.criteria.txt.toLowerCase();
+                const emailsToShow = this.emails.filter((email) => {
+                    return (
+                        email.txt.toLowerCase().includes(searchStr) ||
+                        (email.subject.toLowerCase().includes(searchStr) && email.status.includes(status))
+                    );
+                });
+                return emailsToShow;
+            }
+        },
     },
-  },
-  components: {
-    emailList,
-    emailSideRuler,
-    emailFilter,
-  },
+    components: {
+        emailList,
+        emailSideRuler,
+        emailFilter,
+    },
 };
